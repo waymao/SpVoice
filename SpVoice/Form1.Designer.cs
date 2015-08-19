ad6d7222c96097ca5aef45b7dc0ea60d40fbe684
tonm04@163.com <tonm04@163.com>
2015-08-19 12:08:12 +0800

    dd

---
+++ SpVoice/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SpVoice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.taktTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.voiceRate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.语音选择 = new System.Windows.Forms.Label();
+            this.SelectVoice = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 13);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(602, 224);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 308);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 308);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // taktTime
+            // 
+            this.taktTime.Location = new System.Drawing.Point(124, 266);
+            this.taktTime.Margin = new System.Windows.Forms.Padding(4);
+            this.taktTime.Name = "taktTime";
+            this.taktTime.Size = new System.Drawing.Size(55, 23);
+            this.taktTime.TabIndex = 3;
+            this.taktTime.Text = "4000";
+            this.taktTime.TextChanged += new System.EventHandler(this.taktTime_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 270);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "运行时间间隔:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "(毫秒)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "语速:";
+            // 
+            // voiceRate
+            // 
+            this.voiceRate.Location = new System.Drawing.Point(456, 266);
+            this.voiceRate.Name = "voiceRate";
+            this.voiceRate.Size = new System.Drawing.Size(55, 23);
+            this.voiceRate.TabIndex = 7;
+            this.voiceRate.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(518, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "(范围:1-10)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.语音选择);
+            this.groupBox1.Controls.Add(this.SelectVoice);
+            this.groupBox1.Location = new System.Drawing.Point(13, 244);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(602, 57);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+            // 语音选择
+            // 
+            this.语音选择.AutoSize = true;
+            this.语音选择.Location = new System.Drawing.Point(175, 27);
+            this.语音选择.Name = "语音选择";
+            this.语音选择.Size = new System.Drawing.Size(70, 14);
+            this.语音选择.TabIndex = 1;
+            this.语音选择.Text = "语音选择:";
+            // 
+            // SelectVoice
+            // 
+            this.SelectVoice.FormattingEnabled = true;
+            this.SelectVoice.Location = new System.Drawing.Point(248, 21);
+            this.SelectVoice.Name = "SelectVoice";
+            this.SelectVoice.Size = new System.Drawing.Size(143, 22);
+            this.SelectVoice.TabIndex = 0;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 356);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.voiceRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.taktTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLog);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IT事务管理系统_待办事务提醒";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox taktTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox voiceRate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox SelectVoice;
+        private System.Windows.Forms.Label 语音选择;
+    }
+}
+
---
+++ SpVoice/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using SpeechLib;
+using System.Speech.Synthesis;
+using System.Threading;
+using log4net.Core;
+using log4net;
+using log4net.Config;
+using System.IO;
+using Maticsoft.DBUtility;
+using System.Web;
+using System.Net;
+
+namespace SpVoice
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger("test.logger");
+        private bool logWatching = true;
+        private bool runing;
+        private log4net.Appender.MemoryAppender logger;
+        private Thread logWatcher;
+        private Thread threadOne;
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            this.Closing += new CancelEventHandler(Form1_Closing);
+            logger = new log4net.Appender.MemoryAppender();
+            log4net.Config.BasicConfigurator.Configure(logger);
+            logWatcher = new Thread(new ThreadStart(LogWatcher));
+            logWatcher.Start();
+
+        }
+
+        void Form1_Closing(object sender, CancelEventArgs e)
+        {
+            runing = false;
+            logWatching = false;
+            logWatcher.Join();
+            if (threadOne != null && threadOne.IsAlive)
+            {
+                threadOne.Join();
+            }
+
+        }
+
+        delegate void delOneStr(string log);
+        void AppendLog(string _log)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                delOneStr dd = new delOneStr(AppendLog);
+                txtLog.Invoke(dd, new object[] { _log });
+            }
+            else
+            {
+                StringBuilder builder;
+                //设置窗口显示日志的长度
+                if (txtLog.Lines.Length > 990)
+                {
+                    builder = new StringBuilder(txtLog.Text);
+                    try
+                    {
+                        int txtlog2;
+                        txtlog2 = txtLog.Text.IndexOf('\r', 3000);
+                        builder.Remove(0, txtlog2 + 2);
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error(ex.Message);
+                    }
+                    builder.Append(_log);
+                    txtLog.Clear();
+                    log.Info("清空窗口日志!");
+                }
+                else
+                {
+                    txtLog.AppendText(_log);
+                }
+            }
+        }
+        private void LogWatcher()
+        {
+            while (logWatching)
+            {
+                try
+                {
+                    LoggingEvent[] events = logger.GetEvents();
+                    if (events != null && events.Length > 0)
+                    {
+                        logger.Clear();
+                        foreach (LoggingEvent ev in events)
+                        {
+                            string line = ev.TimeStamp.ToString(" yyyy-MM-dd HH:mm:ss.fff") + " [" + ev.Level + "] " + ev.RenderedMessage + "\r\n";//定义日志格式
+                            AppendLog(line);
+                        }
+                    }
+                    Thread.Sleep(500);
+                }
+                catch (Exception ex)
+                {
+                    log.Error(ex.Message);
+                }
+            }
+        }
+
+        private void Action()
+        {
+            runing = true;
+            threadOne = new Thread(Run);
+            threadOne.Start();
+        }
+        private void Run()
+        {
+            string sql = "SELECT * FROM (select ROWNUM RN,  a.main_title ,b.template_type,c.username,a.createddate, case when (select max(cl_datetime) from flow_node where flow_id = a.flow_id) is null then a.createddate   else (select max(cl_datetime) from flow_node where flow_id = a.flow_id)  end modifieddate,B.DAIBAN_WARNTIME ,d.clr,d.node_id,a.main_sid from flow_maindoc a,it_task_flowtemp b,users c ,flow_node d where   a.main_mission_id=b.flow_mission_mid and d.clr=c.userid and a.flow_id=d.flow_id  and  d.step_status=2 ) T WHERE  to_char((to_date(T.modifieddate,'yyyy-MM-dd hh24:mi:ss')+(T.DAIBAN_WARNTIME*60)/(24*60*60)))<TO_CHAR(sysdate, 'yyyy-MM-dd hh24:mi:ss')";
+            //模板审核
+            string sql2 = "select rownum rn, t.* from (SELECT a.template_name ,a.template_type,b.USERNAME,a.createddate FROM IT_TASK_FLOWTEMP a ,USERS b,flow_maindoc c ,flow_node d  WHERE (  c.flow_id=d.flow_id and  a.sid=c.main_sid and d.step_status=2 and  a.status=1 and  d.clr=b.userid and  a.template_name is not null and a.flow_mission_mid is not null) order by a.createddate desc)t";
+            DataTable dt = new DataTable();
+            while (runing)
+            {
+                try
+                {
+                    dt = DbHelperOra.Query(sql).Tables[0];
+                    Monitor.Enter(this.threadOne);//锁定，保持同步
+                    if (dt.Rows.Count > 0)
+                    {
+                        foreach (DataRow dr in dt.Rows)
+                        {
+                            if (runing)
+                            {
+                                string notice = "事务名称：" + dr["main_title"].ToString() + "，处理人：" + dr["username"].ToString();
+                                log.Info(notice);
+                                Play(notice);
+                                string mbsql = "select mobile from users where userid in(select USERID from IT_TASK_DEPARTMENT t WHERE IS_SONDEPARTMENT_LEADER=1 AND SON_DEPARTMENTID=(select SON_DEPARTMENTID from IT_TASK_DEPARTMENT where USERID='" + dr["clr"].ToString() + "') union select '" + dr["clr"].ToString() + "' USERID from dual)";
+                                DataTable dt3 = DbHelperOra.Query(mbsql).Tables[0];
+                                if (dt3.Rows.Count > 0)
+                                {
+                                    foreach (DataRow dr2 in dt3.Rows)
+                                    { }
+                                }
+                                string mob = "18666279916,15818724380";
+                                string sql4 = " select count(*) from IT_TASK_SMS where main_sid='" + dr["main_sid"].ToString() + "' and node_id='" + dr["node_id"].ToString() + "'";
+                                DataTable dt2 = DbHelperOra.Query(sql4).Tables[0];
+                                if (int.Parse(dt2.Rows[0][0].ToString()) == 0)
+                                {
+                string sql3 = @"insert into INFOSMS(sysid,moduleid,instanceid,timecreate,status,sms_id,sms_to,sms_content,sms_jgdm, sms_lsh,sms_yxjb,sms_shyh)values('gop','42AC49A441DA45D9BFB63AF762B20653', SYS_guid(),sysdate,0,SYS_guid(),'" + mob + @"', '" + notice + @"', '999,999',SEQ_INFOSMS_LSH.Nextval, 3, '')";
+                                    string sql5 = "insert into IT_TASK_SMS(main_sid,node_id,clr)values('" + dr["main_sid"].ToString() + "','" + dr["node_id"].ToString() + "','" + dr["clr"].ToString() + "')";
+                                    DbHelperOra.ExecuteSql(sql3);
+                                    DbHelperOra.ExecuteSql(sql5);
+                                    log.Info("发送短信通知...");
+                                }
+                            }
+                            else
+                            {
+                                log.Info("正在停止...");
+                                break;
+                            }
+                            Thread.Sleep(3000);
+                        }
+                    }
+                    else
+                    {
+                        log.Info(Thread.CurrentThread.Name + "没有待办！");
+                    }
+
+                    Monitor.Exit(this.threadOne);//取消锁定
+                    Thread.Sleep(Convert.ToInt32(taktTime.Text));//线程运行时间间隔
+                }
+                catch (Exception ex)
+                { log.Error(ex.Message); }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = true;
+            log.Info("线程正在启动");
+            Action();
+        }
+
+
+        private void Play(object text)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            PromptBuilder builder = new PromptBuilder(new System.Globalization.CultureInfo("zh-CN"));
+            synth.SetOutputToDefaultAudioDevice();
+            if (SelectVoice.Items.Count > 0)
+            {
+                synth.SelectVoice(SelectVoice.SelectedItem.ToString()); //语音选择
+            }
+            synth.Rate = Convert.ToInt32(voiceRate.Text);//语速
+            builder.AppendText(text.ToString());
+            synth.Speak(builder);
+            synth.Dispose();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            runing = false;
+
+
+            if (threadOne != null && threadOne.IsAlive)
+            {
+
+                threadOne.Join();
+            }
+            else
+            {
+                threadOne.Abort();
+            }
+
+            log.Info("线程已停止");
+
+            this.button1.Enabled = true;
+            this.button2.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.button1.Enabled = true;
+            this.button2.Enabled = false;
+            SpeechSynthesizer synth2 = new SpeechSynthesizer();
+            InstalledVoice[] Voices = synth2.GetInstalledVoices().ToArray();
+            if (Voices.Length > 0)
+            {
+                for (int i = 0; i < Voices.Length; i++)
+                {
+                    SelectVoice.Items.Add(Voices[i].VoiceInfo.Name);
+                }
+                SelectVoice.SelectedItem = Voices[0].VoiceInfo.Name;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void taktTime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+    }
+}
